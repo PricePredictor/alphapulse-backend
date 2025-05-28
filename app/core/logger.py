2a826af94d6ed0f026ec127a601bce1141f6553b
PricePredictor <terraceliquorromeoville@gmail.com>
2025-05-27 19:19:37 -0500

    Update logger.py

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,6 +1,9 @@
+import os
 from loguru import logger
-import sys
+
+# Ensure logs folder exists
+os.makedirs("logs", exist_ok=True)
 
 logger.remove()
-logger.add(sys.stdout, format="{time} {level} {message}", level="INFO", colorize=True)
+logger.add(sys.stdout, level="INFO", colorize=True, format="{time} {level} {message}")
 logger.add("logs/runtime.log", rotation="1 MB", retention="7 days", level="INFO")
