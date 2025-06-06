437d77bf0b6381768e5075655222877b04291d52
PricePredictor <terraceliquorromeoville@gmail.com>
2025-06-06 18:36:31 -0500

    Create test_top_movers.py

---
+++ scripts/test_top_movers.py
@@ -0,0 +1,7 @@
+from scripts.top_movers import get_daily_pct_change, TICKERS
+import json
+
+result = get_daily_pct_change(TICKERS)
+sorted_tickers = sorted(result.items(), key=lambda x: x[1], reverse=True)
+print("Top Gainers:", json.dumps(sorted_tickers[:5], indent=2))
+print("Top Losers:", json.dumps(sorted_tickers[-5:][::-1], indent=2))
